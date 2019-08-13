2a45d76d80cf06abe9eecbaed358f257032adaa3
Piotr Jurczynski <pjurczynski@gmail.com>
2019-08-13 23:40:05 +0200

    return one number

--- src/reverse-polish-notation.spec.ts
+++ src/reverse-polish-notation.spec.ts
@@ -1,5 +1,11 @@
+import { reversePolishNotation } from './reverse-polish-notation';
+
 describe('ReversePolishNotation', () => {
   it('should return 0 on empty string', () => {
     expect(reversePolishNotation('')).toEqual(0);
+  });
+
+  it('should return a number if only one number is in a string', () => {
+    expect(reversePolishNotation('9')).toEqual(9);
   });
 });
