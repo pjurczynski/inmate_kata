a43fda062b52cf9b73c024182fde51cf972fb7ed
Piotr Jurczynski <pjurczynski@gmail.com>
2019-08-14 22:12:23 +0200

    feat: throw error if only two numbers

--- src/reverse-polish-notation.ts
+++ src/reverse-polish-notation.ts
@@ -1,3 +1,8 @@
 export function reversePolishNotation(_x: string): number {
+  const parts: string[] = _x.split(' ');
+  if (parts.length === 2) {
+    throw new Error('no operator');
+  }
+
   return parseInt(_x || '0');
 }
