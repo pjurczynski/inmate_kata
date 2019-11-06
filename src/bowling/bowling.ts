81da9c40b3c14c73356233d7b728ef72da88aa01
Piotr Jurczynski <pjurczynski@gmail.com>
2019-11-06 20:55:58 +0100

    refactor: don't mutate original state

--- src/bowling/bowling.ts
+++ src/bowling/bowling.ts
@@ -12,11 +12,17 @@ export function roll(pins: Pins, state: BowlingGameState) {
     return { ...state, rounds: [[pins]] };
   }
 
-  const latestRound = state.rounds.pop() || [];
+  const latestRound = state.rounds[state.rounds.length - 1] || [];
 
   if (latestRound.length === 2) {
-    return { ...state, rounds: [...state.rounds, latestRound, [pins]] };
+    return {
+      ...state,
+      rounds: [...state.rounds.slice(1, state.rounds.length - 1), latestRound, [pins]],
+    };
   }
 
-  return { ...state, rounds: [...state.rounds, [...latestRound, pins]] };
+  return {
+    ...state,
+    rounds: [...state.rounds.slice(1, state.rounds.length - 1), [...latestRound, pins]],
+  };
 }
