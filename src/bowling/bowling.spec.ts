b97465bfabeb4a7b39d33d94df31f375c6548d72
Piotr Jurczynski <pjurczynski@gmail.com>
2019-11-08 21:09:59 +0100

    test: last round additional roll

--- src/bowling/bowling.spec.ts
+++ src/bowling/bowling.spec.ts
@@ -37,5 +37,18 @@ describe('Bowling game', () => {
 
       expect(newState).toEqual({ rounds: [[10], [1]] });
     });
+
+    it.skip('in last round, you can score 2 tens', () => {
+      const state: BowlingGameState = {
+        rounds: [[10], [10], [10], [10], [10], [10], [10], [10], [10], [10]],
+      };
+      const pins = 10;
+
+      const newState = roll(pins, state);
+
+      expect(newState).toEqual({
+        rounds: [[10], [10], [10], [10], [10], [10], [10], [10], [10], [10, 10]],
+      });
+    });
   });
 });
